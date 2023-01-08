8a499637baf9afb4ab8d5e6bf11b7c6ad4a2d8cb
Pasha Bogdan <pashabogdan94@gmail.com>
2023-01-08 21:39:16 +0300

    задача15и13

---
+++ задача17/Program.cs
@@ -0,0 +1,39 @@
+﻿// 
+
+int getUserValue(string message)
+{
+    Console.WriteLine(message);
+    int result = int.Parse(Console.ReadLine());
+    return result
+}
+
+int getQuarterNumber(int x, int y)
+{
+    int quarter = 0;
+    if (x > 0 && y >0)
+    {
+        quarter = 1;
+    }
+    else if (x < 0 && y > 0)
+    {
+        quarter = 2;
+    }
+    else if (x < 0 && y < 0)
+    {
+        quarter = 3;
+    }
+    else if (x > 0 && y < 0)
+    {
+        quarter = 4;
+    }
+    return quarter
+}
+
+int userX = getUserValue("Введите X");
+int userY = getUserValue("Введите Y");
+int quarter = getQuarterNumber(userX,userY);
+if (userX != 0 && userY != o)
+{
+    int quarter = getQuarterNumber(userX, userY);
+    Console.WriteLine($"Точка[{x}],{y}] находится в {quarter} четверти");
+}
---
+++ задача21/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и 
+// находит расстояние между ними в 2D пространстве.
+// A (3,6); B (2,1) -> 5,09 
+// A (7,-5); B (1,-1) -> 7,21
+
+int getUserValue(string message)
+{
+    Console.WriteLine(message);
+    int result = int.Parse(Console.ReadLine());
+    return result;
+}
+double getDistanceCoordinate (int userAx, int userAy, int userBx, int userBy)
+{
+    double result = Math.Sqrt(Math.Pow((userAx - userBx), 2) + Math.Pow((userAy - userBy), 2));
+    return result;
+}
+int userAx = getUserValue("Введите X");
+int userAy = getUserValue("Введите Y");
+
+int userBx = getUserValue("Введите X");
+int userBy = getUserValue("Введите Y");
+
+double distance = getDistanceCoordinate(userAx, userAy, userBx, userBy);
+Console.WriteLine($"Расстояние между точками {distance}");
